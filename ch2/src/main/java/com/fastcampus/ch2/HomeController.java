fdc319f209e2269d41bcee885db1bb491eee4e2d
김성진 <gimseongjin@gimseongjin-ui-MacBookAir.local>
2024-01-05 19:14:18 +0900

    redirect & forward

--- ch2/src/main/java/com/fastcampus/ch2/HomeController.java
+++ ch2/src/main/java/com/fastcampus/ch2/HomeController.java
@@ -2,12 +2,25 @@ package com.fastcampus.ch2;
 
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.Arrays;
 
 @Controller
 public class HomeController {
     @RequestMapping("/")
     public String main() {
         return "index";
+    }
+
+    @GetMapping("/test")
+    public String test(Model m)
+    {
+        m.addAttribute("lastName", "kim");
+        m.addAttribute("firstName", "seongjin");
+        m.addAttribute("list", Arrays.asList("aaa", "bbb","ccc","ddd","eee"));
+        return "leaf";
     }
 }
--- ch2/src/main/java/com/fastcampus/ch2/LoginController.java
+++ ch2/src/main/java/com/fastcampus/ch2/LoginController.java
@@ -6,27 +6,72 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import javax.servlet.http.HttpServletRequest;
 import java.net.URLEncoder;
 
+
+/*
 @Controller
-@RequestMapping("/login") // 공통적으로 맵핑될 URI
+@RequestMapping("/login")
 public class LoginController {
-    @GetMapping("/login") // GET login/login
+    @GetMapping("/login")
     public String showLogin() {
         return "login";
     }
 
-    @PostMapping("/login") // POST login/login
-    public String login(String id, String pwd, Model m) throws Exception {
+
+    @PostMapping("/login")
+    public String login(String id, String pwd, Model m, RedirectAttributes ra, HttpServletRequest req) throws Exception {
 
         if(loginCheck(id, pwd)) {
             m.addAttribute("Id", id);
             m.addAttribute("Pwd", pwd);
             return "userInfo";
         } else {
-            String msg = URLEncoder.encode("id 또는 pwd가 일치하지 않습니다.", "utf-8");
-            return "redirect:/login/login?msg=" + msg; // redirect는 Get 요청을 수행함.
+            String msg = "id 또는 pwd가 일치하지 않습니다.";
+            // ra.addAttribute("msg", msg);
+            ra.addFlashAttribute("msg", msg);
+
+            return "redirect:/login/login";
+        }
+    }
+
+    private boolean loginCheck(String id, String pwd) {
+        return id.equals("asdf") && pwd.equals("1234");
+    }
+}
+*/
+
+
+
+/////////////////////////////
+
+
+
+
+
+@Controller
+@RequestMapping("/login")
+public class LoginController {
+    @GetMapping("/login")
+    public String showLogin() {
+        return "login";
+    }
+
+
+    @PostMapping("/login")
+    public String login(String id, String pwd,HttpServletRequest req) throws Exception {
+
+        if(loginCheck(id, pwd)) {
+            req.setAttribute("Id", id);
+            req.setAttribute("Pwd", pwd);
+            return "userInfo";
+        } else {
+            String msg = "id 또는 pwd가 일치하지 않습니다.";
+            req.setAttribute("msg", msg);
+            return "forward:/";
         }
     }
 
@@ -35,3 +80,19 @@ public class LoginController {
     }
 
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
