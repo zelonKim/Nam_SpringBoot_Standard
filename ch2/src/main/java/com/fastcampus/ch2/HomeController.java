7abc6ed42f1047d5a055fa2878cc75b100552c9a
김성진 <gimseongjin@gimseongjin-ui-MacBookAir.local>
2024-01-07 21:54:17 +0900

    thymeleaf

--- ch2/src/main/java/com/fastcampus/ch2/HomeController.java
+++ ch2/src/main/java/com/fastcampus/ch2/HomeController.java
@@ -6,6 +6,9 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import javax.servlet.ServletContext;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
 import java.util.Arrays;
 
 @Controller
@@ -15,12 +18,24 @@ public class HomeController {
         return "index";
     }
 
-    @GetMapping("/test")
-    public String test(Model m)
+    @GetMapping("/leaf")
+    public String leaf(Model m, HttpServletRequest request)
     {
+        request.setAttribute("year", 2024);
+
+        HttpSession session = request.getSession();
+        session.setAttribute("id", "asdf");
+
+        ServletContext application = session.getServletContext();
+        application.setAttribute("email", "ksz1860@fastcampus.com");
+
+
+
         m.addAttribute("lastName", "kim");
         m.addAttribute("firstName", "seongjin");
         m.addAttribute("list", Arrays.asList("aaa", "bbb","ccc","ddd","eee"));
+        m.addAttribute("bno", 123);
+        m.addAttribute("user", new User("aaa",20));
         return "leaf";
     }
 }
